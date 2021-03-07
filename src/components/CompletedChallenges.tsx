9188e9d4482659b3e4fa6e9a30a2b09a451eb64a
Joel Oliveira <joeldorosarioo@gmail.com>
2021-03-07 11:47:47 -0300

    Creating the Completed Challenges component

---
+++ src/components/CompletedChallenges.tsx
@@ -0,0 +1,10 @@
+import styles from '../stylesheets/components/CompletedChallenges.module.css';
+
+export function CompletedChallenges () {
+	return (
+		<div className={ styles.completdChallengesContainer }>
+			<span>Desafios completos</span>
+			<span>5</span>
+		</div>
+	);
+}
