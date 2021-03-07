383c629eb0331fcab9569b247f5b06a6fedfaf76
Joel Oliveira <joeldorosarioo@gmail.com>
2021-03-07 11:06:02 -0300

    Creating the basic structure of the project

---
+++ src/pages/_app.tsx
@@ -0,0 +1,7 @@
+import '../stylesheets/globalStylesheets.css';
+
+function MyApp ({ Component, pageProps }) {
+	return <Component { ...pageProps } />
+};
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,24 @@
+import { ExperienceBar } from '../components/ExperienceBar';
+import { Profile } from '../components/Profile';
+import { CompletedChallenges } from '../components/CompletedChallenges';
+import { Countdown } from '../components/Countdown';
+
+import styles from '../stylesheets/pages/Home.module.css';
+
+export default function Home() {
+	return (
+		<div className={ styles.container }>
+			<ExperienceBar />
+
+			<section>
+				<div>
+					<Profile />
+					<CompletedChallenges />
+					<Countdown />
+				</div>
+
+				<div></div>
+			</section>
+		</div>
+	);
+}
